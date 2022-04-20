7f3f7572225127453f1f00950cba0eb23d5aa571
Paul-Eirik <pmelhus@gmail.com>
2022-04-20 19:35:20 -0400

    done

--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
@@ -1,16 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-function Message({ size }) {
+function Message({ featherCount, size }) {
+
+    const [sizeClass, setSizeClass] = useState('');
+
+    const [message, setMessage] = useState('');
+
+    useEffect(() =>{
+        if (featherCount <= 0)
+            setMessage('Oh my! Your bird is naked!');
+        else if (featherCount >= 10) {
+            setMessage('Full turkey!');
+        } else {
+            setMessage('Coming along...');
+        }
+    }, [featherCount])
 
     useEffect(() => {
-        console.log('Message', size);
-    },[size])
+      console.log('PictureDisplay size', size);
+      let cname = '';
+      switch (size) {
+          case 'm':
+              cname = 'medium';
+              break;
+          case 'l':
+              cname = 'large';
+              break;
+          case 'xl':
+              cname = 'xlarge';
+              break;
+          default:
+              cname = 'small';
+              break;
+      }
+      console.log(cname);
+      setSizeClass(cname);
+  }, [size]);
 
-    return (
-        <div className="message medium">
-            (Oh my! Your bird is naked!)
-        </div>
-    );
+  return (
+    <div className={`message ${sizeClass}`}>
+        {message}
+    </div>
+);
 };
 
 export default Message;
--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/PictureDisplay.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/PictureDisplay.js
@@ -9,7 +9,7 @@ import feather7 from "../images/feather7.svg";
 import feather8 from "../images/feather8.svg";
 import feather9 from "../images/feather9.svg";
 import featherA from "../images/featherA.svg";
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 const feathers = [
     feather1,
@@ -26,9 +26,28 @@ const feathers = [
 
 function PictureDisplay ({ size, featherCount, featherColors }) {
 
-useEffect(() => {
-        console.log('PictureDisplay', size, featherCount, featherColors);
-    }, [size, featherCount, featherColors]);
+  const [sizeClass, setSizeClass] = useState('');
+
+  useEffect(() => {
+    console.log('PictureDisplay size', size);
+    let cname = '';
+    switch (size) {
+        case 'm':
+            cname = 'medium';
+            break;
+        case 'l':
+            cname = 'large';
+            break;
+        case 'xl':
+            cname = 'xlarge';
+            break;
+        default:
+            cname = 'small';
+            break;
+    }
+    console.log(cname);
+    setSizeClass(cname);
+}, [size]);
 
   // TODO: Wrap in useEffect
   const colors = [];
@@ -38,7 +57,7 @@ useEffect(() => {
   }
 
   return (
-    <div className={`image-area medium`}>
+    <div className={`image-area ${sizeClass}`}>
       {colors.map((c, i) =>
         <img
             key={feathers[i]}
