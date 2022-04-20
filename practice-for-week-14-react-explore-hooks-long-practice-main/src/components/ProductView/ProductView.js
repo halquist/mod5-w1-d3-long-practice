01545f8e8c4ab055a42f0e98fabd742d0ddbe8f2
jon <jon.halquist@gmail.com>
2022-04-20 17:39:00 -0500

    phase 2 done

--- practice-for-week-14-react-explore-hooks-long-practice-main/src/components/ProductView/ProductView.js
+++ practice-for-week-14-react-explore-hooks-long-practice-main/src/components/ProductView/ProductView.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
+import {useState} from 'react';
 import './ProductView.css'
 
 function ProductView({ products }) {
 
     // TODO: Replace with state variable
-    const sideOpen = true;
+    const [sideOpen, setSideOpen] = useState(true);
+    const [selectedProduct, setSelectedProduct] = useState('');
+    const [isSelected, setIsSelected] = useState(false);
 
     return (
         <div className="product-view">
@@ -17,7 +20,10 @@ function ProductView({ products }) {
                         <ProductListItem
                             key={item.id}
                             product={item}
-                            onClick={() => console.log('SELECT PRODUCT', item)}
+                            isSelected={item === selectedProduct}
+                            onClick={() => {
+                                setSelectedProduct(item);
+                            }}
                         />
                     )}
                 </div>
@@ -25,11 +31,11 @@ function ProductView({ products }) {
             <div className="product-side-panel">
                 <div className="product-side-panel-toggle-wrapper">
                     <div className="product-side-panel-toggle"
-                         onClick={() => console.log('TOGGLE SIDE PANEL')}>
+                         onClick={() => setSideOpen(!sideOpen)}>
                         {sideOpen ? '>' : '<'}
                     </div>
                 </div>
-                <ProductDetails visible={sideOpen} />
+                <ProductDetails visible={sideOpen} product={selectedProduct} />
             </div>
         </div>
     );
